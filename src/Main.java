165d3db7496881f2bac640605020b69d9f221b6b
unknown <BT_3W2_20@BKTECH.LOCAL>
2022-11-14 09:30:28 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args){
+        for (int i = 5; i < 10; i++) {
+            System.out.print(i + " ");
+        }
+
+        int i=5;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+
+
+        /*for(int i=1; i<=10;i++){
+            System.out.println(i);
+        }
+        for(int i=1; i<11;i++){
+            System.out.println(i);
+        }
+        for(int i=15; i>=0; i--){
+            System.out.println(i);
+        }
+        for(int i=15; i>-1; i--){
+            System.out.println(i);
+        }
+        for(int i=0; i<=15; i++){
+            if (i%2==1){
+                System.out.println(i);
+            }
+        }
+        for (int i=1; i<=15; i+=2){
+            System.out.println(i);
+        }
+
+         */
+    }
+}
